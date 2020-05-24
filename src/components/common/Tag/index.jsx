82f1ab8f2dca09f32e6881203aceff6725370f63
Rahul Nath <rahulpnath@gmail.com>
2020-05-24 20:40:40 +1000

    Single color for tags

--- src/components/common/Tag/index.jsx
+++ src/components/common/Tag/index.jsx
@@ -1,30 +1,29 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
 import { StyledTag } from './styles'
 
 export const Tag = ({ tag, link }) => {
-  const {
-    tags: { edges },
-  } = useStaticQuery(graphql`
-    query {
-      tags: allTagsYaml {
-        edges {
-          node {
-            title
-            bg
-            color
-          }
-        }
-      }
-    }
-  `)
+  // const {
+  //   tags: { edges },
+  // } = useStaticQuery(graphql`
+  //   query {
+  //     tags: allTagsYaml {
+  //       edges {
+  //         node {
+  //           title
+  //           bg
+  //           color
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
 
-  const {
-    node: { bg, color, title },
-  } = edges.find(({ node: { title } }) => title === tag)
+  // const {
+  //   node: { bg, color, title },
+  // } = edges.find(({ node: { title } }) => title === tag)
   return (
-    <StyledTag bg={bg} color={color} to={link}>
-      {title}
+    <StyledTag bg={'#263238'} color={'#fff'} to={link}>
+      {tag}
     </StyledTag>
   )
 }
