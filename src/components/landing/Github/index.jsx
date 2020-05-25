a64cf7ab5bab074fb912a63fc7d21a326f8aac87
Rahul Nath <rahulpnath@gmail.com>
2020-05-26 06:44:40 +1000

    start from smakosh

---
+++ src/components/landing/Github/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { Project, GithubIcon, Container, Subtitle } from 'components/common'
+
+export const Github = () => {
+  const { github } = useStaticQuery(
+    graphql`
+      query {
+        github {
+          repositoryOwner(login: "smakosh") {
+            repositories(
+              first: 7
+              orderBy: { field: STARGAZERS, direction: DESC }
+            ) {
+              edges {
+                node {
+                  id
+                  name
+                  url
+                  description
+                  stargazers {
+                    totalCount
+                  }
+                  forkCount
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <Container>
+      <Subtitle>Open Source</Subtitle>
+      <Project
+        icon={GithubIcon}
+        type="Github"
+        projects={github.repositoryOwner.repositories.edges}
+        link="https://github.com/smakosh"
+        color="#000"
+        github
+      />
+    </Container>
+  )
+}
