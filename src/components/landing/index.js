edfd6f9df791fbe4341b276a928387ca34f54296
Rahul Nath <rahulpnath@gmail.com>
2020-05-26 14:26:46 +1000

    Removed unnecessay files

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,8 +1,5 @@
-export * from './Github'
 export * from './Intro'
 export * from './Popular'
-export * from './Services'
 export * from './Skills'
-export * from './Testimonials'
 export * from './Work'
 
