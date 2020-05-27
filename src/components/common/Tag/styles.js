cb455fb54a7eb73f7fa473eff4b69e8e84e9f265
Rahul Nath <rahulpnath@gmail.com>
2020-05-28 05:56:34 +1000

    tag and code formatting

--- src/components/common/Tag/styles.js
+++ src/components/common/Tag/styles.js
@@ -1,16 +1,17 @@
-import styled from 'styled-components'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
 
 export const StyledTag = styled(Link)`
   padding: 0.2rem 1rem;
-  background: ${({ bg }) => bg};
-  color: ${({ color }) => color};
+  color: ${({ bg }) => bg} !important;
   border-radius: 50px;
   transition: 0.3s all;
   opacity: 1;
-
+  border: 1px solid ${({ bg }) => bg};
   &:hover {
-    opacity: 0.8;
+    color: ${({ color }) => color} !important;
+    background: ${({ bg }) => bg};
+    opacity: 1;
     transition: 0.3s all;
   }
 `
