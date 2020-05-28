a583104a7cb7651c384e5fb05b64d96c36db7fdc
Rahul Nath <rahulpnath@gmail.com>
2020-05-29 08:51:34 +1000

    reset font size

--- src/providers/typography.js
+++ src/providers/typography.js
@@ -1,6 +1,5 @@
 import Typography from 'typography';
 import typographyTheme from 'typography-theme-github';
-typographyTheme.baseFontSize = '18px';
 const typography = new Typography(typographyTheme)
 
 export default typography
